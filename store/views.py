bf6efa1384d45ed07b17a95387169031b4fc09b3
Jani Paasiluoto <jani.paasiluoto@innokasmedical.fi>
2021-10-13 15:22:08 +0300

    added signup function

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Q
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -26,3 +29,17 @@ def bookListView(request):
             Q(titleicontains=title) & Q(authoricontains=author) & Q(genre__icontains=genre))
 
     return render(request, template_name, context=context) 
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
