ea6ba2b4f7bd421f79931e7c7e87e6153266a2c2
Jani Paasiluoto <jani.paasiluoto@innokasmedical.fi>
2021-10-13 15:21:31 +0300

    added signup url

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 from store.views import *
 
 urlpatterns = [
     path('', index, name="index"),
-    
+    url(r'^signup/$', signup, name='signup'),
     
 ]
